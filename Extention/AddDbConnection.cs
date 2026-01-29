9925b6eda9a4bb7a615543203576da14a54da74e
ncuxoseeliq <150700636+Inseliq@users.noreply.github.com>
2026-01-29 09:39:17 +0300

    upd db connection

---
+++ Extention/AddDbConnection.cs
@@ -0,0 +1,16 @@
+using HsoPkipt.Data;
+using Microsoft.EntityFrameworkCore;
+
+public static class AddDbConnection
+{
+    public static IServiceCollection AddDb(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<AppDbContext>(
+            options =>
+            {
+                options.UseNpgsql(configuration.GetConnectionString("Postgres"));
+            }
+            );
+        return services;
+    }
+}
